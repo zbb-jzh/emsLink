ca08d30933ca2ac95521aa4f048efac7ea3cdf5a
zbb-jzh <603845887@qq.com>
2018-11-26 16:55:12 +0800

    112601

--- emsLink/WebContent/AdminLTE/consumer/js/consumer_au.js
+++ emsLink/WebContent/AdminLTE/consumer/js/consumer_au.js
@@ -1,37 +1,4 @@
 
-var setting = {
-        treeNodeKey: "id",
-        treeNodeParentKey: "parentId",
-
-//        view: {
-//            dblClickExpand: true
-//        },
-        check: {
-            enable: true,
-            chkStyle: "radio",  //单选框
-            radioType: "all"   //对所有节点设置单选
-//            chkStyle: "checkbox",
-//            chkboxType: {
-//                "Y": "ps", "N": "ps"
-//            }
-        },
-        data: {
-            simpleData: {
-                enable: true,
-                idKey: "id",
-                pIdKey: "parentId"
-
-            },
-            key: {
-                children: "nodes"
-            }, keep: {
-                parent: true,
-                open: true
-            }
-        }
-    };
-var zTreeObj = null;
-
 var vm = avalon.define({
 	$id:'consumerau',
 	consumerId:getUrlData('id'),
@@ -72,10 +39,7 @@ var vm = avalon.define({
         }
 		if(vm.consumerId)
 		{
-			if(vm.consumerId == vm.consumer.parentId){
-				alert('自己不能选择自己');
-				return;
-			}
+			
 			$.ajax({
 			    url: "../consumer/doUpdate",    //请求的url地址
 			    dataType: "json",   //返回格式为json
@@ -113,32 +77,6 @@ var vm = avalon.define({
 			});
 		}
 	},
-	getConsumerTreeList: function () {
-    	$.ajax({
-		    url: "/link/consumer/doTree",    //请求的url地址
-		    dataType: "json",   //返回格式为json
-		    data: {},    //参数值
-		    type: "post",   //请求方式
-		    success: function(res) {
-		    	if (res.status == 100) {
-		    		//vm.shopCategoryList = res.data;
-		    		console.log(res.data)
-		            zTreeObj = $.fn.zTree.init($("#goodsCateory"), setting, res.data);
-		            zTreeObj.expandAll(true);
-		            
-		            if(vm.consumer.id){
-		            	zTreeObj.checkNode(zTreeObj.getNodeByParam("id", vm.consumer.parentId, null), true, true);
-		            }
-		            
-                }else{
-                	alert(res.data);
-                }
-		    },
-		    error: function() {
-		    	console.log('error');
-		    }
-		});
-    },
 	goback:function()
 	{
 		window.location.href = '#/consumer/list';
@@ -146,5 +84,4 @@ var vm = avalon.define({
 });
 
 vm.getConsumer();
-vm.getConsumerTreeList();
 avalon.scan();
